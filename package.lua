01dc5122698ff482e3e1037ae946b547ca5b6318
Lautenschlager-id <tai.romani@hotmail.com>
2020-04-30 12:49:52 -0300

    bump up to version v6.0.0

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
 	name = "Lautenschlager-id/transfromage",
-	version = "5.0.0",
+	version = "6.0.0",
 	homepage = "https://github.com/Lautenschlager-id/Transfromage",
 	dependencies = {
 		"creationix/base64",
