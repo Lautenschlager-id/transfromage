82db5978d78005599d17b724af988fd820a14c11
Lautenschlager <tai.romani@hotmail.com>
2020-10-03 16:28:06 -0300

    adjust emitters

--- src/libs/api/Client/utils/emit.lua
+++ src/libs/api/Client/utils/emit.lua
@@ -1,3 +1,17 @@
+local Client = require("Client/init")
+
+local timer = require("timer")
+
+-- Optimization --
+local assert = assert
+local coroutine_makef = coroutine.makef
+local coroutine_resume = coroutine.resume
+local coroutine_running = coroutine.running
+local coroutine_yield = coroutine.yield
+local timer_clearTimeout = timer.clearTimeout
+local timer_setTimeout = timer.setTimeout
+------------------
+
 --[[@
 	@name on
 	@desc Sets an event emitter that is triggered everytime a specific behavior happens.
@@ -8,6 +22,7 @@
 Client.on = function(self, eventName, callback)
 	return self.event:on(eventName, coroutine_makef(callback))
 end
+
 --[[@
 	@name once
 	@desc Sets an event emitter that is triggered only once a specific behavior happens.
@@ -18,6 +33,18 @@ end
 Client.once = function(self, eventName, callback)
 	return self.event:once(eventName, coroutine_makef(callback))
 end
+
+--[[@
+	@name emit
+	@desc Emits an event.
+	@desc See the available events in @see Events. You can also create your own events / emitters.
+	@param eventName<string> The name of the event.
+	@param ...?<*> The parameters to be passed during the emitter call.
+]]
+Client.emit = function(self, eventName, ...)
+	return self.event:emit(eventName, ...)
+end
+
 --[[@
 	@name waitFor
 	@desc Yields the running coroutine and will resume it when the given event is triggered.
@@ -31,15 +58,26 @@ end
 	@returns ... The parameters of the event.
 ]]
 Client.waitFor = function(self, eventName, timeout, predicate)
-	return self.event:waitFor(eventName, timeout, predicate)
-end
---[[@
-	@name emit
-	@desc Emits an event.
-	@desc See the available events in @see Events. You can also create your own events / emitters.
-	@param eventName<string> The name of the event.
-	@param ...?<*> The parameters to be passed during the emitter call.
-]]
-Client.emit = function(self, eventName, ...)
-	return self.event:emit(eventName, ...)
+	local coro = coroutine_running()
+
+	local eventCallback
+	eventCallback = function(...)
+		if not predicate or predicate(...) then
+			if timeout then
+				timer_clearTimeout(timeout)
+			end
+
+			self.event:removeListener(eventName, eventCallback)
+			return assert(coroutine_resume(coro, true, ...))
+		end
+	end
+
+	self:on(eventName, eventCallback)
+
+	timeout = timeout and timer_setTimeout(timeout, function()
+		self:removeListener(eventName, eventCallback)
+		return assert(coroutine_resume(coro, false))
+	end)
+
+	return coroutine_yield()
 end
