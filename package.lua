672cb13e14265d91b515a7da1bd72d2894d28d7f
Lautenschlager <tai.romani@hotmail.com>
2023-07-31 10:35:05 -0300

    fix license, add package

---
+++ package.lua
@@ -0,0 +1,17 @@
+return {
+	name = "Lautenschlager-id/transfromage",
+	version = "9.0.0",
+	homepage = "https://github.com/Lautenschlager-id/Transfromage",
+	dependencies = {
+		"creationix/base64",
+		"creationix/coro-http",
+		"luvit/secure-socket",
+		"luvit/net"
+	},
+	tags = { "transformice", "bot", "game", "api", "lua", "luvit", "fromage", "transfromage" },
+	license = "MIT",
+	author = "Lautenschlager-id",
+	files = {
+		"*.lua"
+	}
+}
