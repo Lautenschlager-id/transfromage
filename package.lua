8429f087891ff6225bdc3c0f87a8310f2d40bc8b
Lautenschlager <tai.romani@hotmail.com>
2024-09-22 21:58:01 -0300

    update ip / ports endpoint

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
 	name = "Lautenschlager-id/transfromage",
-	version = "9.0.0",
+	version = "9.0.1",
 	homepage = "https://github.com/Lautenschlager-id/Transfromage",
 	dependencies = {
 		"creationix/base64",
--- transfromage/libs/api/enum/enums/settings.lua
+++ transfromage/libs/api/enum/enums/settings.lua
@@ -53,7 +53,7 @@ enum.setting = {
 ]]
 enum.url = enum {
 	translation  = "http://transformice.com/langues/tfm-%s.gz",
-	gameSettings = "https://cheese.formice.com/api/tfm/ip",
+	gameSettings = "https://athes.dev/api/tfm/ip",
 	apiPackage   = "https://raw.githubusercontent.com/Lautenschlager-id/Transfromage/master/\z
 		package.lua"
 }
