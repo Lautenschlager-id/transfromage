ab4fd7baf0dfe2596343f5c80a0b3025e87e2c4b
Jedi Fay <174175668+JediFay@users.noreply.github.com>
2025-01-21 23:57:19 +0100

    Update enterRoom.lua

--- transfromage/libs/api/Client/Sender/room/enterRoom.lua
+++ transfromage/libs/api/Client/Sender/room/enterRoom.lua
@@ -10,7 +10,7 @@ local identifier = require("api/enum").identifier.room
 	@param roomName<string> The name of the room.
 	@param isSalonAuto?<boolean> Whether the change room must be /salonauto or not. @default false
 ]]
-client.enterRoom = function(self, roomName, roomPassword, isSalonAuto)
+Client.enterRoom = function(self, roomName, roomPassword, isSalonAuto)
 	if not roomPassword then roomPassword = '' end
 	self.mainConnection:send(identifier,
 		ByteArray:new():writeUTF(''):writeUTF(roomName):writeUTF(roomPassword):writeBool(isSalonAuto))
