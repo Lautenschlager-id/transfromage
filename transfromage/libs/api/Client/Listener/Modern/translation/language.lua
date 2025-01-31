ea074587c1ec1d9737e67b92e6c69dd524b7e0a4
Lautenschlager <tai.romani@hotmail.com>
2025-01-30 23:10:24 -0300

    keep standards for new newLanguage event

--- transfromage/libs/api/Client/Listener/Modern/translation/language.lua
+++ transfromage/libs/api/Client/Listener/Modern/translation/language.lua
@@ -4,12 +4,12 @@ local onNewLanguage = function(self, packet, connection, identifiers)
 		@desc Triggered when a language is changed.
 		@param language<string> The code of the language.
 		@param country<string> The code of the country.
-		@param readRight<boolean> Whether the language is read left to right or not.
-		@param readSpecialChar<boolean> Whether the language has special characters or not.
+		@param isRTL<boolean> Whether the language is read left to right or not.
+		@param hasSpecialCharacters<boolean> Whether the language has special characters or not.
 	 ]]
 	local language, country = packet:readUTF(), packet:readUTF()
-	local readRight, readSpecialChar = packet:readBool(), packet:readBool()
-	self.event:emit("newLanguage", language, country, readRight, readSpecialChar)
+	local isRTL, hasSpecialCharacters = packet:readBool(), packet:readBool()
+	self.event:emit("newLanguage", language, country, isRTL, hasSpecialCharacters)
 end
 
 return { onNewLanguage, 176, 5 }
